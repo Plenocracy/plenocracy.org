80e6add8b9cc2e313ff16af26338f197d52e4264
wildone <wildone@users.noreply.github.com>
2021-01-31 14:26:34 +0000

    deploy: 73d27bdcc6aa51c906a958dcafded5552e8775f4

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Plenocracy",
+        "excerpt":"Welcome to Plenocracy!  ","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "https://plenocracy.org/blog/2021/02/01/plenocracy",
+        "teaser": null
+      }]
